34b0b3e67b2163890ef189c8582fd83989a2a226
Huge Huang <hhuang@netflix.com>
2013-11-10 11:00:08 -0800

    update template

--- app/public/js/main.js
+++ app/public/js/main.js
@@ -7,9 +7,29 @@
         list = d.querySelector('ol[name="results"]'),
         template = new Template('#template-search-result');
 
+    function getSearchResults(query, cb) {
+        cb({
+            name: 'express',
+            stars: 30,
+            author: 'visionmedia',
+            followers: 2000,
+            ci: {
+                success: true,
+                time: '2013 11 08'
+            }
+        });
+    }
+
     function render(query) {
-        template.one('.l-box').innerHTML = query;
-        template.prependTo(list);
+        getSearchResults(query, function (data) {
+            template.one('h2').textContent = data.name;
+            template.one('i.fa-star').textContent = data.stars;
+            template.one('i.fa-github').textContent = 'visionmedia';
+            template.one('i.fa-users').textContent = '30';
+            template.one('i.test-result').classList.add(data.ci.success ? 'fa-check' : 'fa-times');
+            template.one('i.test-result').textContent = data.ci.time;
+            template.prependTo(list);
+        });
     }
 
     input.getKeys().subscribe(render);
