750cc7b110d235386bff22a1774a5aa8b7f1725c
Huge Huang <hhuang@netflix.com>
2013-11-09 23:15:13 -0800

    tweak run script

--- run.js
+++ run.js
@@ -3,12 +3,13 @@
 process.setMaxListeners(0);
 var Package = require('./lib/package').Package;
 var source = require('./lib/source');
-var testPackages = source.packagesList;
+var testPackages = source.packagesList.slice(0, 100);
 var SIZE = source.packagesList.length;
 var fs = require('fs');
-var BATCH_NUMBER = 100;
+var BATCH_NUMBER = 10;
 var INTERVAL = 3000;
 var batchRun = 1;
+var results = {};
 
 var timeoutId = setTimeout(function batch() {
     runner(batchRun);
@@ -17,13 +18,6 @@ var timeoutId = setTimeout(function batch() {
 }, INTERVAL);
 
 function runner(index) {
-    results = {};
-
-    if (index * BATCH_NUMBER > SIZE) {
-        clearTimeout(timeoutId);
-        return;
-    }
-
     (function (batch) {
         console.log('batch %d', batch);
         testPackages.slice((index-1) * BATCH_NUMBER, index * BATCH_NUMBER).
@@ -35,9 +29,11 @@ function runner(index) {
                     var p = new Package(package);
                     p.getData(function (e, data) {
                         results[data.name] = data;
-                        if (i === context.length-1) {
+                        if (i === context.length-1 && batch === 10) {
                             console.log('finished batch %d', batch);
+                            clearTimeout(timeoutId);
                             fs.writeFileSync('./output/' + batch + '.json', JSON.stringify(results));
+                            console.log('we can closed process now, process.exit?');
                         }
                     });
                 }
