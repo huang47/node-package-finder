3667ffc3a402ea023b9f4c50542784d22ce66bce
Huge Huang <hhuang@netflix.com>
2013-11-10 15:15:04 -0800

    add voteko

--- app/routes/index.js
+++ app/routes/index.js
@@ -4,7 +4,5 @@
  */
 
 exports.index = function(req, res){
-  // http://blog.nodeknockout.com/post/35364532732/protip-add-the-vote-ko-badge-to-your-app
-  var voteko = '<iframe src="http://nodeknockout.com/iframe/go-taiwanese" frameborder=0 scrolling=no allowtransparency=true width=115 height=25></iframe>';
-  res.render('index', { title: 'npm smart searc', voteko: voteko });
+  res.render('index', { title: 'node package finder' });
 };
