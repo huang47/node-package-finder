1184eac71f094ac4c9c278d90097df8e2b755f6d
Shao-Hua Kao <skao>
2013-11-10 15:37:17 -0800

    10 results

--- app/routes/search.js
+++ app/routes/search.js
@@ -6,5 +6,5 @@ const searchHelper = require('../../src/helper/searchHelper');
 exports.search = function(req, res) {
   var query = req.params.query;
   res.setHeader('Content-Type', 'application/json');
-  res.end('searchResultCb(' + JSON.stringify(searchHelper.searchPackage(query)) + ')');
+  res.end('searchResultCb(' + JSON.stringify(searchHelper.searchPackage(query).slice(0, 10)) + ')');
 };
